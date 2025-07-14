e816f0aba0b00a39a107930e06557e091673011d
Jakub Jastrzebski <jakub.jastrzebski99@gmail.com>
2025-07-14 23:38:35 +0200

    array job ga

--- array_job_lem.sh
+++ array_job_lem.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --partition lem-cpu    
 #SBATCH --job-name=renos_array
-#SBATCH --time=12:00:00         #time limit HH:MM:SS
+#SBATCH --time=06:00:00         #time limit HH:MM:SS
 #SBATCH --nodes=1             #reserve nodes
 #SBATCH --ntasks-per-node=1              #task per all nodes
 #SBATCH --cpus-per-task=128   #number of threads per task
-#SBATCH --mem=20gb               #memory per node
+#SBATCH --mem=25gb               #memory per node
 #SBATCH --gres=storage:lustre:1
 #SBATCH --mail-user=jakub.jastrzebski99@gmail.com
 #SBATCH --mail-type=ALL,ARRAY_TASKS    #ARRAY_TASKS to receive emails for each array task
-#SBATCH --array=1-50         #Array job specification - run jobs with indices 1-100
+#SBATCH --array=1-100         #Array job specification - run jobs with indices 1-100
 
 source /usr/local/sbin/modules.sh
 module load Python/3.11.3-GCCcore-12.3.0
 module load pybind11/2.11.1-GCCcore-12.3.0
 source ~/renos/bin/activate
 
-ALGO="ACO"
+ALGO="GA"
 RESULT_DIR="LeNet4_run_aray_${SLURM_ARRAY_TASK_ID}"
 RESULT_DIR_HOME="/home/jjastrz9/tmp/ReNOS/data/${ALGO}_run_array"
 
