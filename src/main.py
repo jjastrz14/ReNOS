c7fe29a3e6009172aedaa95b7e903850b51913d0
Jakub Jastrzebski <jakub.jastrzebski99@gmail.com>
2025-07-22 15:13:48 +0200

    another run on lem

--- src/main.py
+++ src/main.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         sol_per_pop = 512, #30,
         n_parents_mating= 100, #Number of solutions to be selected as parents.
         keep_parents= -1 , #10, # -1 keep all parents, 0 means do not keep parents, 10 means 10 best parents etc
-        parent_selection_type= "tournament", # The parent selection type. Supported types are sss (for steady-state selection), rws (for roulette wheel selection), sus (for stochastic universal selection), rank (for rank selection), random (for random selection), and tournament (for tournament selection). k = 3 for tournament, can be changed
+        parent_selection_type= "sss", # The parent selection type. Supported types are sss (for steady-state selection), rws (for roulette wheel selection), sus (for stochastic universal selection), rank (for rank selection), random (for random selection), and tournament (for tournament selection). k = 3 for tournament, can be changed
         n_generations = 1000, #800,
         mutation_probability = .4, #some exploration, so don’t kill mutation completely.
         crossover_probability = .9, #outlier genes to propagate = crossover must dominate.
